b011d4df4472591f5d2fccac4c21486dd70cb1ad
WinHtut <winhtutonline@gmail.com>
2021-11-06 12:36:16 +0630

    TCP server and client
    not included income data decompress and decrypt in client side

---
+++ TCPserverAndClient-5/TCP5Server/ToSendFromServer.py
@@ -0,0 +1,11 @@
+
+
+class ToFindData:
+    def __init__(self,inputdata:str):
+        self.inputData = inputdata
+
+    def dbFinder(self)-> str:
+        if self.inputData == "WINHTUT":
+            data:str ="NationalCyberCity"
+
+        return data
